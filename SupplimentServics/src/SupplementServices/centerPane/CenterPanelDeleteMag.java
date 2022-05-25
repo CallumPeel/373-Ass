967c7941bd72a333e260b7d7e8dee9888d89c2fe
Callum <callum.epeel@gmail.com>
2022-05-26 03:31:17 +0800

    Selected Mag works

---
+++ SupplimentServics/src/SupplementServices/centerPane/CenterPanelDeleteMag.java
@@ -0,0 +1,82 @@
+/*
+ * Student Name: Callum Peel
+ * Student ID: 34217062
+ */
+package SupplementServices.centerPane;
+
+import SupplementServices.BackEnd;
+import SupplementServices.FrontEndGUI;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author callu
+ */
+public class CenterPanelDeleteCustomer extends CenterPanelEdit {
+
+    VBox mainBox;
+    HBox buttons;
+    Label question;
+    Button yesButton, noButton;
+
+    public CenterPanelDeleteCustomer(BackEnd backEnd, FrontEndGUI frontEnd) {
+        super(backEnd, frontEnd);
+        this.question = new Label("Delete Customer?");
+        this.yesButton = new Button("YES");
+        this.yesButton.setOnAction(e -> {
+            saveChange();
+            System.out.println("Delete on\n");
+        });
+        this.noButton = new Button("NO");
+        this.noButton.setOnAction(e -> {
+            this.frontEnd.refresh();
+            System.out.println("Delete off\n");
+        });
+        this.buttons = new HBox(this.yesButton, this.noButton);
+        this.mainBox = new VBox(question, buttons);
+        this.buttons.setAlignment(Pos.CENTER);
+        this.buttons.setPadding(new Insets(20));
+        this.buttons.setSpacing(20);
+        this.mainBox.setAlignment(Pos.CENTER);
+
+        setCenter();
+        setPane();
+    }
+
+    private void setCenter() {
+        this.centerSectionPane.setCenter(this.mainBox);
+    }
+
+    private void saveChange() {
+        System.out.println("Save button clicked on delete");
+        try {
+            this.backEnd.removeCustomer(this.backEnd.getCustomer(this.frontEnd.customerSelected));
+            this.frontEnd.setDefaultSelectedCustomer();
+        } catch (Exception e) {
+            System.out.println("Something Went Wrong");
+        }
+        this.frontEnd.refresh();
+    }
+
+    @Override
+    public void onSaveChangesButtonClick() {
+        System.out.println("Save button clicked on delete");
+        try {
+            this.backEnd.removeCustomer(this.backEnd.getCustomer(this.frontEnd.customerSelected));
+            this.frontEnd.setDefaultSelectedCustomer();
+        } catch (Exception e) {
+            System.out.println("Something Went Wrong");
+        }
+        this.frontEnd.refresh();
+    }
+    
+    @Override
+    public void setBottomPane() {
+        
+    }
+}
